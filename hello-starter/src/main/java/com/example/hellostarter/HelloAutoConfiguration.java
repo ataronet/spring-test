62ea43892d0b47ff374cd5182b674d5baace2fe3
Atara Shachar <ashachar@atlassian.com>
2020-04-26 19:10:04 +1000

    Adding your own conditional class

--- hello-starter/src/main/java/com/example/hellostarter/HelloAutoConfiguration.java
+++ hello-starter/src/main/java/com/example/hellostarter/HelloAutoConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 public class HelloAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
+    @Conditional(OnValidHelloPrefixCondition.class) // attach your own conditional class
     public HelloService helloService(HelloProperties helloProperties){
         return new ConsoleHelloService(
                 helloProperties.getPrefix(),
---
+++ hello-starter/src/main/java/com/example/hellostarter/OnValidHelloPrefixCondition.java
@@ -0,0 +1,34 @@
+package com.example.hellostarter;
+
+import org.springframework.boot.autoconfigure.condition.ConditionMessage;
+import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
+import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+// Write your own conditional class
+public class OnValidHelloPrefixCondition extends SpringBootCondition {
+    private static final String PROPERTY_NAME = "hello.prefix";
+
+    @Override
+    public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        Environment environment = context.getEnvironment();
+        ConditionMessage.Builder condition = ConditionMessage.forCondition("ValidHelloPrefix");
+        if (environment.containsProperty(PROPERTY_NAME)) {
+            String value = environment.getProperty(PROPERTY_NAME);
+            if(value != null && !value.isEmpty() && Character.isUpperCase(value.charAt(0))){
+                return ConditionOutcome.match(
+                        condition.available(String.format("Prefix '%s' starts with capital letter, yey!", value))
+                );
+            } else {
+                return ConditionOutcome.noMatch(
+                        condition.because(String.format("Value for '%s' does not start with capital letter", PROPERTY_NAME))
+                );
+            }
+        }
+        return ConditionOutcome.match(
+                condition.because(String.format("Value for '%s' was not found, loading defaults", PROPERTY_NAME))
+        );
+    }
+}

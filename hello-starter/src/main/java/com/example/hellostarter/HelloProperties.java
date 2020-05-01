613dde4a15177a6a74bfebaabe007e563fd78a00
Atara Shachar <ashachar@atlassian.com>
2020-05-01 14:18:14 +1000

    Use of ConstructorBinding annotation and delete the setters of HelloProperties

--- hello-starter/src/main/java/com/example/hellostarter/HelloProperties.java
+++ hello-starter/src/main/java/com/example/hellostarter/HelloProperties.java
@@ -1,28 +1,29 @@
 package com.example.hellostarter;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties("hello") //section in the application.yml
+@ConstructorBinding
 public class HelloProperties {
     /**
      * Configuration of the hello service, with defaults
      */
-    private String prefix = "Hello";
-    private String suffix = "!";
+    final String DEFAULT_PREFIX = "hello";
+    final String DEFAULT_SUFFIX = "!";
+
+    private String prefix;
+    private String suffix;
+
+    public HelloProperties(String prefix, String suffix) {
+        this.prefix = (prefix != null)? prefix: DEFAULT_PREFIX;
+        this.suffix = (suffix != null)? suffix: DEFAULT_SUFFIX;
+    }
 
     public String getPrefix() {
         return prefix;
     }
-
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
-    }
-
     public String getSuffix() {
         return suffix;
-    }
-
-    public void setSuffix(String suffix) {
-        this.suffix = suffix;
     }
 }
